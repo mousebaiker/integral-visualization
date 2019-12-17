a30df724308bb27fabc0e4fc0ae0613b002e45ce
mousebaiker <mousebaiker@gmail.com>
2019-12-18 01:56:33 +0300

    Set up Github page

---
+++ docs/cubes.js
@@ -0,0 +1,12 @@
+function loadFile(filePath) {
+  var result = null;
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("GET", filePath, false);
+  xmlhttp.send();
+  if (xmlhttp.status==200) {
+    result = xmlhttp.responseText;
+  }
+  return result;
+}
+
+console.log(loadFile("https://github.com/mousebaiker/integral-visualization/blob/master/docs/abses.txt"))
